ba12b82b543ab1b69b05bab8d2870a16ea8d4569
neetuchauhan <neetuchauhan0421#gmail.com>
2020-06-12 17:20:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -14,9 +14,8 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          Learn React In Vipul Domain
         </a>
       </header>
     </div>
