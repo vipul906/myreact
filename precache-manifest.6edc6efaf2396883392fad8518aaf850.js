15844c22071edd4cce853af4f64834de16ab8805
neetuchauhan <neetuchauhan0421#gmail.com>
2020-06-12 17:15:55 +0530

    Updates

---
+++ precache-manifest.6edc6efaf2396883392fad8518aaf850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea660a92fc6aafc64f53b6197955cd5f",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "1c610ad1b0af9fd1018f",
+    "url": "/myreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d328829215299d6f95a8",
+    "url": "/myreact/static/js/2.0c7d6832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.0c7d6832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c610ad1b0af9fd1018f",
+    "url": "/myreact/static/js/main.1fa54df1.chunk.js"
+  },
+  {
+    "revision": "d577c4e6413c6e5c0b75",
+    "url": "/myreact/static/js/runtime-main.ab4f6a90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
